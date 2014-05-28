df85ee513f1d441f2acb6e4abacda30a443bd8ce
Bill Xiong <billxiong99@gmail.com>
2014-05-27 17:22:29 -0700

    updated build.bat to run with all flags

--- A1/build.bat
+++ A1/build.bat
@@ -1,8 +1,9 @@
 @echo off
+del cbc.exe
 gplex.exe Cblexer.lex
 echo -------Gplex Completed, Starting Gppg-------
 gppg /gplex CbParser.y > main.cs
 echo -------Gppg Completed-------
 csc /r:QUT.ShiftReduceParser.dll cbc.cs Cblexer.cs main.cs
 echo -------All Done!-------
-cbc test.cs
+cbc -debug -tokens test.cs
