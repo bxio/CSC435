808f9917f9d906f0d073c595e5ab10c7d7d2abc4
Bill Xiong <billxiong99@gmail.com>
2014-05-27 20:19:54 -0700

    Added test file to better reference.

---
+++ A1/test.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Test{
+	public override void test(){
+
+	}
+	//this is a single line comment
+	public virtual int getZero(){
+		return 0;
+	}
+	/*This is a
+	multiple
+	line
+	comment */
+	public static void Main(String[] args){
+
+		int x = 2;
+		//qwerty
+		if(x==2){
+			int y = 3;
+		}
+	}
+}
