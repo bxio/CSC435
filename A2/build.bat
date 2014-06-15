79cca8bb0df118af2e8fdfe95928db97379eda68
Bill Xiong <billxiong99@gmail.com>
2014-06-15 15:48:36 -0700

    updated build file for namespace

--- A2/build.bat
+++ A2/build.bat
@@ -6,6 +6,6 @@ gplex CbLexer.lex
 echo -------Gplex Completed, Starting Gppg-------
 gppg /gplex CbParser.y
 echo -------Gppg Completed, compiling csc-------
-csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs CbAST.cs CbType.cs CbTopLevel.cs CbVisitor.cs CbPrVisitor.cs cbc.cs
+csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs CbAST.cs CbType.cs CbTopLevel.cs CbVisitor.cs CbPrVisitor.cs CbTypeVisitor.cs cbc.cs
 pause
-cbc -ast CbExample.cs
+cbc -ast -ns CbExample.cs
