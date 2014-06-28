abb93b211f204ca334679c782c4ebeaec0040358
Bill Xiong <billxiong99@gmail.com>
2014-06-28 16:56:25 -0700

    Updated build file and added A3 hints.

--- A3/build.bat
+++ A3/build.bat
@@ -1,1 +1,8 @@
-csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs  CbTopLevel.cs CbAST.cs CbType.cs CbVisitor.cs CbPrVisitor.cs CbTLVisitor.cs CbSymTab.cs cbc.cs
+del CbLexer.cs CbParser.cs cbc.exe
+echo Old files removed!
+gplex CbLexer.lex
+echo -------Gplex Completed, Starting Gppg-------
+gppg /gplex CbParser.y
+echo -------Gppg Completed, compiling csc-------
+csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs  CbTopLevel.cs CbAST.cs CbType.cs CbVisitor.cs CbPrVisitor.cs CbTLVisitor.cs CbTcVisitor.cs TCVisitor1.cs CbSymTab.cs cbc.cs
+pause
