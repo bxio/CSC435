d753b053a9568c38cba4d44138c39c1f20b22a88
Bill Xiong <billxiong99@gmail.com>
2014-06-29 16:53:19 -0700

    Updated build.bat for TCVisitor2

--- A3/build.bat
+++ A3/build.bat
@@ -1,6 +1,8 @@
 del CbLexer.cs CbParser.cs cbc.exe
+@echo off
 gplex CbLexer.lex
 echo -------Gplex Completed, Starting Gppg-------
 gppg /gplex CbParser.y
 echo -------Gppg Completed, compiling csc-------
-csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs  CbTopLevel.cs CbAST.cs CbType.cs CbVisitor.cs CbPrVisitor.cs CbTLVisitor.cs TCVisitor1.cs CbSymTab.cs cbc.cs
+csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs  CbTopLevel.cs CbAST.cs CbType.cs CbVisitor.cs CbPrVisitor.cs CbTLVisitor.cs TCVisitor1.cs TCVisitor2.cs CbSymTab.cs cbc.cs
+cbc.exe -ns FullTest.cs
