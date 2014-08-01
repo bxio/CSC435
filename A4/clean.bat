7f018a2d59ec2316686d56877c8b6ea672b816a6
Bill Xiong <billxiong99@gmail.com>
2014-07-31 18:18:26 -0700

    Added more batch files

---
+++ A4/clean.bat
@@ -0,0 +1,10 @@
+@echo off
+del "cbc.exe"
+del "cbc.pdb"
+del *.ll
+del "Fibs.s"
+del "Fibs"
+del "cbc.pdb"
+
+echo ------- Project cleaned! -------
+
--- A4/test.bat
+++ A4/test.bat
@@ -1,9 +1,16 @@
+@echo off
 del "cbc.exe"
 del "Fibs.ll"
 del "Fibs.s"
 del "Fibs"
-@echo off
 echo ------- Compiling -------
 csc /debug:full /r:QUT.ShiftReduceParser.dll CbLexer.cs CbParser.cs  CbTopLevel.cs CbAST.cs CbType.cs CbVisitor.cs CbPrVisitor.cs CbTLVisitor.cs CbSymTab.cs CbTypeCheckVisitor1.cs CbTypeCheckVisitor2.cs LLVM.cs LLVM-Arrays.cs LLVM-ConstantHandling.cs LLVM-CreateClassDefn.cs LLVM-Definitions.cs LLVM-SSAMethods.cs LLVM-UtilityMethods.cs LLVMVisitor1.cs LLVMVisitor2.cs LLVM-WriteMethods.cs cbc.cs
-echo ------- Executing -------
-cbc.exe Fibs.cs
+echo ------- Testing Conditionals -------
+cbc.exe ./tests/test_conditional.cb
+echo ------- Testing Basic Expressions -------
+cbc.exe ./tests/test_incdec.cb
+echo ------- Testing Unary Expressions -------
+cbc.exe ./tests/test_unary.cb
+echo ------- Testing While -------
+cbc.exe ./tests/test_while.cb
+
